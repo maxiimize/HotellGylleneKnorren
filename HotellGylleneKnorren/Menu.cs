d9294ef0cda8a457c5ceae088fc4543330228d66
maxiimize <max.b@hotmail.se>
2024-12-30 12:25:47 +0100

    Added menu in which navigation is done by arrows up and down, then confirmation with enter. Also made the marked option show as green and the whole menu being shown with a white border in the middle of the console.

---
+++ HotellGylleneKnorren/Menu.cs
@@ -0,0 +1,59 @@
+﻿namespace HotellGylleneKnorren
+{
+    internal class Menu
+    {
+        public static void DrawMenu(string[] options, int selectedOption)
+        {
+            int boxWidth = 30;
+            int boxHeight = options.Length + 4;
+
+            // Ritar topp kanten
+            Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + new string('=', boxWidth));
+
+            for (int i = 0; i < boxHeight; i++)
+            {
+                if (i == 1 || i == boxHeight - 2)
+                {
+                    Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + "=" + new string(' ', boxWidth - 2) + "=");
+                }
+                else if (i >= 2 && i <= options.Length + 1)
+                {
+                    int optionIndex = i - 2;
+                    string optionText = options[optionIndex];
+
+                    if (optionIndex == selectedOption)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + $"= {optionText.PadRight(boxWidth - 4)} =");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + $"= {optionText.PadRight(boxWidth - 4)} =");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + "=" + new string(' ', boxWidth - 2) + "=");
+                }
+            }
+
+            // Ritar botten kanten
+            Console.WriteLine(new string(' ', (Console.WindowWidth - boxWidth) / 2) + new string('=', boxWidth));
+        }
+
+        public static void HandleMenuSelection(string selectedOption)
+        {
+            Console.Clear();
+            Console.WriteLine($"You selected: {selectedOption}");
+
+            if (selectedOption == "Exit")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Press any key to return to the menu...");
+            Console.ReadKey(true);
+        }
+    }
+}
--- HotellGylleneKnorren/Program.cs
+++ HotellGylleneKnorren/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] menuOptions =
+        {
+            "Add Room",
+            "Add Customer",
+            "Add Booking",
+            "View Rooms",
+            "View Customers",
+            "View Bookings",
+            "Exit"
+        };
+
+            int selectedOption = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Menu.DrawMenu(menuOptions, selectedOption);
+
+                var key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedOption = (selectedOption == 0) ? menuOptions.Length - 1 : selectedOption - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedOption = (selectedOption == menuOptions.Length - 1) ? 0 : selectedOption + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        Menu.HandleMenuSelection(menuOptions[selectedOption]);
+                        break;
+                }
+            }
         }
+
+        
+
+        
     }
 }
