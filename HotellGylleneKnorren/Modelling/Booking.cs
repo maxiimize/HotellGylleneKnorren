88ff10cfbc52642102e2906c2a68366adfac39c4
maxiimize <max.b@hotmail.se>
2024-12-30 11:46:56 +0100

    Added classes for modelling

---
+++ HotellGylleneKnorren/Modelling/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellGylleneKnorren.Modelling
+{
+    internal class Booking
+    {
+        public int BookingId { get; set; }
+        public int CustomerId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ HotellGylleneKnorren/Modelling/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotellGylleneKnorren.Modelling
+{
+    internal class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomType { get; set; }
+        public int MaxExtraBeds { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
